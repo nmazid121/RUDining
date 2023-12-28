f8c44de15569228680893cb641a926787a6599b2
nmazid121 <nmazid121@gmail.com>
2023-12-28 00:45:03 -0500

    calm lil test

--- src/ArasScraperForTesting.py
+++ src/ArasScraperForTesting.py
@@ -1,3 +1,5 @@
+import pymongo
+from pymongo import MongoClient 
 import requests
 from bs4 import BeautifulSoup
 
@@ -17,7 +19,7 @@ def scrape_aras_hot_chicken():
                 price = item.find_next('div', class_='AixKWd')  # Adjust the selector based on the actual structure
 
                 if price:
-                    print(f"{item.get_text().strip()}: {price.get_text().strip()}")
+                    print(f"Food item: {item.get_text().strip()}: {price.get_text().strip()}")
                 else:
                     print(f"{item.get_text().strip()}: Price not found")
 
@@ -26,4 +28,18 @@ def scrape_aras_hot_chicken():
     except requests.RequestException as e:
         print(f"An error occurred: {e}")
 
+
 scrape_aras_hot_chicken()
+arasClient = pymongo.MongoClient("mongodb+srv://chaz:chaz@rudining.h9cvsrb.mongodb.net/?retryWrites=true&w=majority")
+# mydb = arasClient["myDatabase"]
+print(arasClient.list_database_names())
+mydb = arasClient["TestDatabase"]
+mycol = mydb["TestCollection"]
+mydict = [
+    { "foodName": "BABY SPINACHBROCCOLI", "price" : "$1" },
+          {"foodName": "Cheese", "price" : "$100"}
+]
+
+
+mycol.insert_many(mydict)
+
